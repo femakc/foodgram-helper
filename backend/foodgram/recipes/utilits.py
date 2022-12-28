da3b54cbde5ab5710c8ef354445f9d9471be5e11
Максим Федоров <femakc@yandex.ru>
2022-12-28 15:51:05 +0300

    review v2

--- backend/foodgram/recipes/utilits.py
+++ backend/foodgram/recipes/utilits.py
@@ -7,9 +7,8 @@ TRANS_DICT = {
 
 def make_send_file(ingredient):
     with open('shop_list_file.txt', 'w') as shop_list_file:
-        shop_list_file.write('Список ингредиентов:\n')
+        shop_list_file.write('Список ингредиентов:\n\n')
         for i in ingredient:
-            # shop_list_file.write('Список ингредиентов:\n')
             for key, value in i.items():
                 shop_list_file.write(f'{TRANS_DICT[key]} - {value}\n')
                 if key == 'amount':
